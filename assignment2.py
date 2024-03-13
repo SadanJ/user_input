d071ef7bd24951aad19a525ad2705e430be2a915
SadanJ <danieljoshua36@yahoo.com>
2024-03-13 13:02:26 +0300

    Added Python Script for Week 2 assignment

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+# Creating a list called my_list
+my_list = []
+
+# Appendinf the elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserting the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# Extending the list
+my_list.extend([50, 60, 70])
+
+# Removing the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("Index of 30 in my_list is", index_of_30)
+
